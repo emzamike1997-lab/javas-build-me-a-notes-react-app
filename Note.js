680a962bcd0d124e083478f5c08b83529d02de82
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 13:29:18 +0100

    JAVAS: Add Note.js

--- Note.js
+++ Note.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function Note({ note }) {
-  return (
-    <div>
-      <h2>{note.title}</h2>
-      <p>{note.content}</p>
-    </div>
-  );
+function Note({ note, deleteNote }) {
+    return (
+        <div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+            <button onClick={() => deleteNote(note.id)}>Delete</button>
+        </div>
+    );
 }
 
 export default Note;
