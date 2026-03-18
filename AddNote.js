828944629f0e7f960ce8d7acf420fc8d61167d64
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-18 13:29:19 +0100

    JAVAS: Add AddNote.js

--- AddNote.js
+++ AddNote.js
@@ -18,8 +18,17 @@ function AddNote({ addNote }) {
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
-            <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content" />
+            <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+            />
+            <textarea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                placeholder="Content"
+            />
             <button type="submit">Add Note</button>
         </form>
     );
